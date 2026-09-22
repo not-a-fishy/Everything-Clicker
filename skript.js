bd7513fdb8f11c1974ffb93b41e8ff3ae876eb62
Coder5330 <261540h@student.hci.edu.sg>
2026-09-22 09:56:28 +0800

    Update skript.js

--- skript.js
+++ skript.js
@@ -4,6 +4,12 @@ const moneyPerSecondUI = document.getElementById("money-per-second-ui");
 const upgradesContainer = document.getElementById("upgrades-container");
 const moneyImg = document.getElementById("money-img");
 
+const upgrades = [
+    // stuff here :)
+    // format
+    // ['name', 'description', 'cost', 'amount per click', 'amount per second', 'unlocked (boolean)']
+]
+
 let money = 0;
 let moneyPerClick = 1;
 let moneyPerSecond = 0;
@@ -13,6 +19,19 @@ function update(amount = 0) {
     moneyUI.textContent = `$${money}`;
     moneyPerClickUI.textContent = `$${moneyPerClick} per click`;
     moneyPerSecondUI.textContent = `$${moneyPerSecond} per second`;
+}
+
+function updateUpgrades() {
+    upgrades.forEach((upgrade) => {
+        if (!upgrade.unlocked && money > upgrade.cost) upgrade.unlocked = true;
+    });
+
+    upgrades.forEach((upgrade) => {
+        if (upgrade.unlocked) {
+            const div = document.createElement("div");
+            // finish this
+        }
+    });
 }
 
 moneyImg.addEventListener('click', () => {
