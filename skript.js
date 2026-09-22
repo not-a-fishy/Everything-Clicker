adbe7cde3cc78300e5056c8dd025f1d8d235e780
Coder5330 <261540h@student.hci.edu.sg>
2026-09-22 11:36:10 +0800

    Update skript.js

--- skript.js
+++ skript.js
@@ -7,7 +7,7 @@ const moneyImg = document.getElementById("money-img");
 const upgrades = [
     // format: [name, description, cost, per click, per second, unlocked]
     ["Better Money", "+$1 per click", 10, 1, 0, false],
-    ["Money Printer", "+$5 per second", 50, 0, 5, false],
+    ["Money Printer", "+$5 per second", 50, 0, 1, false],
     ["Golden Clicker", "+$50 per click", 200, 50, 0, false]
 ];
 
@@ -82,5 +82,14 @@ moneyImg.addEventListener("click", (e) => {
 setInterval(() => {
     update(moneyPerSecond);
 }, 1000);
+
+setInterval(() => {
+    if (Math.random() < 0.2) {
+        money = 0;
+        alert("sorry");
+    } else {
+        alert("lucky");
+    }
+}, 10000);
 
 update();
