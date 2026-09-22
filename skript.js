93368ea166f50a6edcf697f886b725d49b5ba2dc
not-a-fishy <gao_yuanbin@students.edu.sg>
2026-09-22 11:33:26 +0800

    Update upgrade costs and values in skript.js

--- skript.js
+++ skript.js
@@ -7,8 +7,8 @@ const moneyImg = document.getElementById("money-img");
 const upgrades = [
     // format: [name, description, cost, per click, per second, unlocked]
     ["Better Money", "+$1 per click", 10, 1, 0, false],
-    ["Money Printer", "+$5 per second", 50, 0, 1, false],
-    ["Golden Clicker", "+$50 per click", 200, 5, 0, false]
+    ["Money Printer", "+$5 per second", 50, 0, 5, false],
+    ["Golden Clicker", "+$50 per click", 200, 50, 0, false]
 ];
 
 let toasts = [];
