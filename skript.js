7419d5db63b4ce12171c78be253df6fb425d2be7
Coder5330 <261540h@student.hci.edu.sg>
2026-09-22 09:47:09 +0800

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,5 +1,32 @@
-const a = 0;
-const i = 1;
-function add() {
-    a += i;
+const moneyUI = document.getElementById("money-ui");
+const moneyPerClickUI = document.getElementById("money-per-click-ui");
+const moneyPerSecondUI = document.getElementById("money-per-second-ui");
+const upgradesContainer = document.getElementById("upgrades-container");
+const moneyImg = document.getElementById("money-img");
+
+let money = 0;
+let moneyPerClick = 1;
+let moneyPerSecond = 0;
+
+function updateUI() {
+    moneyUI.textContent = `$${money}`;
+    moneyPerClickUI.textContent = `$${moneyPerClick} per click`;
+    moneyPerSecondUI.textContent = `$${moneyPerSecond} per second`;
 }
+
+function update() {
+    money += moneyPerSecond;
+    updateUI();
+}
+
+moneyImg.addEventListener(('click', 'tap') => {
+    money += moneyPerClick;
+});
+
+
+
+setInterval(() => {
+
+    update();
+
+}, 1000);
