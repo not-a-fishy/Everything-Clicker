785fd76a5083cfe60880ad7f6aaf7ac0a2d138f1
Coder5330 <261540h@student.hci.edu.sg>
2026-09-22 09:52:44 +0800

    yesa

--- skript.js
+++ skript.js
@@ -4,25 +4,23 @@ const moneyPerSecondUI = document.getElementById("money-per-second-ui");
 const upgradesContainer = document.getElementById("upgrades-container");
 const moneyImg = document.getElementById("money-img");
 
-let money = 0; 
+let money = 0;
 let moneyPerClick = 1;
 let moneyPerSecond = 0;
 
-function updateUI() {
+function update(amount = 0) {
+    money += amount;
     moneyUI.textContent = `$${money}`;
     moneyPerClickUI.textContent = `$${moneyPerClick} per click`;
     moneyPerSecondUI.textContent = `$${moneyPerSecond} per second`;
 }
 
-function update() {
-    money += moneyPerSecond;
-    updateUI();
-}
-
 moneyImg.addEventListener('click', () => {
-    money += moneyPerClick;
+    update(moneyPerClick);
 });
 
 setInterval(() => {
-    update();
+    update(moneyPerSecond);
 }, 1000);
+
+update();
